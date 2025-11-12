b47853f2040ba4bf4c0226a8ccfa4b8eee8e24b7
observauto <felipegaran@observauto.com>
2025-11-12 00:03:14 -0500

    delete

--- src/types/canvas-confetti.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'canvas-confetti' {
-  type Options = Record<string, unknown>;
-  type Confetti = (options?: Options) => unknown;
-
-  const confetti: Confetti;
-  export default confetti;
-}
